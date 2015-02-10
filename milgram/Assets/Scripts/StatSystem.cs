02efe116d26138421461a11d089567ddaff80c88
lendeuel <lendeuel@me.com>
2015-02-09 20:18:04 -0700

    Added a stat system with the initial stats as the ones sam listed on trello

---
+++ milgram/Assets/Scripts/StatSystem.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class StatSystem : MonoBehaviour 
+{
+	public enum Stats
+	{
+		health,
+		willingness,
+		willpower,
+		tolerance
+	};
+
+	private Dictionary<Stats, float> stats;
+
+	void Awake () 
+	{
+		stats = new Dictionary<Stats, float> ();	
+	}
+	
+	public void AddValueToStat(Stats stat, float value)
+	{
+		if (!stats.ContainsKey (stat)) 
+		{
+			stats.Add(stat, value);
+		}
+		else
+		{
+			stats.Add(stat, stats[stat]+value);
+		}
+	}
+
+	public float GetValueForStat(Stats stat)
+	{
+		if(!stats.ContainsKey(stat))
+		{
+			return 0;
+		}
+		return stats [stat];
+	}
+
+	public void SetStats(Dictionary<Stats, float> newDic)
+	{
+		stats = newDic;
+	}
+
+	public Dictionary<Stats, float> GetStats()
+	{
+		return stats;
+	}
+}

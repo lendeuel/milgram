45bafe1259815d1c2afeb7e1c8732a3556366958
lendeuel <lendeuel@me.com>
2015-02-09 19:41:25 -0700

    Drag is simple script that allows dragging, ListenerManager manages all of the drag listeners in the scene, DragListener is an interface that listeners must implement to be recognized by listener manager, simpledraglistener is an abstract class that inherits mono behavior and implements drag listener and manages the contains method assuming a BoxCollider2D, you still have to register yourself if you inherit from it. PrintHiIfDropped is a sample implementation of the listener system that prints hi if a draggable object is dropped in it.

---
+++ milgram/Assets/Scripts/PrintHiIfDropped.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrintHiIfDropped : SimpleDragListener
+{
+	void Start()
+	{
+		GameObject.FindObjectOfType<ListenerManager>().Register(this);
+	}
+
+	override public void OnDrop(GameObject drop)
+	{
+		Debug.Log ("Hi");
+	}
+}

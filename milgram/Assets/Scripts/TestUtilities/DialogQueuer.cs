24c5851cb0e3ad8b70048cd31167298f0c25a46f
lendeuel <lendeuel@me.com>
2015-03-24 20:34:58 -0600

    Restored a change trevor made.

--- milgram/Assets/Scripts/TestUtilities/DialogQueuer.cs
+++ milgram/Assets/Scripts/TestUtilities/DialogQueuer.cs
@@ -24,23 +24,11 @@ public class DialogQueuer : ButtonAction, TextScroller.TextScrollerEndedResponde
 
 	public override void takeAction ()
 	{
-<<<<<<< HEAD
 		s.addStrings(lines);
 		s.endedResponse = this;
 		s.characterToMaterialMapping = characterToMaterialMapping;
-		chatWindow.SetActive (true);
-=======
-		GameObject.FindGameObjectWithTag("DialogHandler").collider.enabled = true;
-		GameObject.FindGameObjectWithTag("ChatWindow").renderer.enabled = true;
-		
-		TextScroller s = GameObject.FindGameObjectWithTag("DialogHandler").AddComponent<TextScroller> ();
-		Button b = GameObject.FindGameObjectWithTag("DialogHandler").gameObject.AddComponent<Button> ();
-		b.action = s;
-		s.linesToLoad = lines;
-		s.endedResponse = this;
-		s.characterToMaterialMapping = characterToMaterialMapping;
 		s.lettersPerSecond = lettersPerSecond;
->>>>>>> 15b4ac7f149c751d0e2a0979161b3d0e7d615a0d
+		chatWindow.SetActive (true);
 	}
 
 	public void textScrollerEnded()

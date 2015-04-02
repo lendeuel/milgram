3db0db06ed8f3a761579e9b97cc47db69ee64aec
TrevorNewell <u0774750@utah.edu>
2015-04-02 16:27:54 -0600

    Created Timer That Properly Displays A Countdown
    Timer now contains a script called Time Manager.  This allows
    specification of a start time.
    There's also a variable for interval.  This controls how many real
    seconds are treated as a single second in the game.
    An interval of 5 means 5 real seconds, are treated as a single second in
    game.
    An interval of .1 means 1 real second is treated as 10 seconds in game.
    Default is at .5 right now.
    
    Also created a SubtractTime script that implements ButtonAction.  I
    currently have one setup on the far left Number as an example.  It
    subtracts 25 minutes currently.

---
+++ milgram/Assets/Scripts/SimpleActions/SubtractTime.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SubtractTime : ButtonAction
+{
+	public int days;
+	public int hours;
+	public int minutes;
+
+	private TimeManager t;
+
+	void Start()
+	{
+		t = FindObjectOfType<TimeManager>();
+	}
+
+	public override void takeAction()
+	{
+		t.SubtractTime(days,hours,minutes);
+	}
+}
---
+++ milgram/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,159 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Timers;
+public class TimeManager : MonoBehaviour
+{
+	public float defaultIntervalInSeconds = 60;
+	private double defaultIntervalInMilliseconds = 0;
+
+	public int startingDays = 0;
+	public int startingHours = 0;
+	public int startingMinutes = 0;
+
+	public Material[] numbers = new Material[10];
+
+	public MeshRenderer[] children = new MeshRenderer[6];
+
+	private TimeSpan ts;
+	private Timer t;
+
+	private Boolean needsUpdate = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// Convert to milliseconds
+		defaultIntervalInMilliseconds = defaultIntervalInSeconds * 1000;
+
+		if (startingMinutes >= 60)
+		{
+			startingHours += startingMinutes/60;
+			startingMinutes = startingMinutes%60;
+		}
+
+		if (startingHours >= 24)
+		{
+			startingDays += startingHours/24;
+			startingHours = startingHours%24;
+		}
+
+		ts = new TimeSpan(startingDays, startingHours, startingMinutes, 0);
+
+		SetNumbers();
+
+		t = new Timer(defaultIntervalInMilliseconds);
+		t.Elapsed += OnTimedEvent;
+		t.Start();
+	}
+
+	void Update()
+	{
+		if (needsUpdate)
+		{
+			SetNumbers();
+		}
+	}
+
+	// This is called every time defaultIntervalInSeconds has passed.  Each time it is called, a minute is taken off the time left.
+	void OnTimedEvent(object source, ElapsedEventArgs e)
+	{
+		SubtractTime(0,0,1);
+	}
+
+	public void SubtractTime(int days, int hours, int minutes)
+	{
+		if (minutes >= 60)
+		{
+			hours -= minutes/60;
+			minutes = minutes%60;
+		}
+		
+		if (hours >= 24)
+		{
+			days -= hours/24;
+			hours = hours%24;
+		}
+
+		ts = ts.Subtract(new TimeSpan(days, hours, minutes, 0, 0));
+
+		startingDays = ts.Days;
+		startingHours = ts.Hours;
+		startingMinutes = ts.Minutes;
+
+		needsUpdate = true;
+	}
+
+	void SetNumbers()
+	{
+		startingDays = ts.Days;
+		startingHours = ts.Hours;
+		startingMinutes = ts.Minutes;
+
+
+		// Set days
+		char[] daysSplit = startingDays.ToString().ToCharArray();
+		int day1 = 0;
+		int day2 = 0;
+		
+		if (daysSplit.Length == 2)
+		{
+			day1 = (int)daysSplit[0] - 48;
+			day2 = (int)daysSplit[1] - 48;
+		}
+		else 
+		{
+			day2 = (int)daysSplit[0] - 48;
+		}
+		
+		children[0].material = numbers[day1];
+		children[1].material = numbers[day2];
+
+
+		// Set hours
+		char[] hoursSplit = startingHours.ToString().ToCharArray();
+		int hour1 = 0;
+		int hour2 = 0;
+		
+		if (hoursSplit.Length == 2)
+		{
+			hour1 = (int)hoursSplit[0] - 48;
+			hour2 = (int)hoursSplit[1] - 48;
+		}
+		else 
+		{
+			hour2 = (int)hoursSplit[0] - 48;
+		}
+		
+		children[2].material = numbers[hour1];
+		children[3].material = numbers[hour2];
+
+
+		// Set minutes
+		char[] minutesSplit = startingMinutes.ToString().ToCharArray();
+		int minute1 = 0;
+		int minute2 = 0;
+		
+		if (minutesSplit.Length == 2)
+		{
+			minute1 = (int)minutesSplit[0] - 48;
+			minute2 = (int)minutesSplit[1] - 48;
+		}
+		else 
+		{
+			minute2 = (int)minutesSplit[0] - 48;
+		}
+		
+		children[4].material = numbers[minute1];
+		children[5].material = numbers[minute2];
+
+
+		needsUpdate = false;
+	}
+
+	void OnApplicationQuit()
+	{
+		t.Stop();
+		t.Dispose();
+	}
+}

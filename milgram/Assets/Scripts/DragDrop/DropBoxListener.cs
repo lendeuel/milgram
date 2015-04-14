de4c5f014880226b154b2d68a8990f1122a0c496
TrevorNewell <u0774750@utah.edu>
2015-04-13 20:48:16 -0600

    Added sound support to the drawer

--- milgram/Assets/Scripts/DragDrop/DropBoxListener.cs
+++ milgram/Assets/Scripts/DragDrop/DropBoxListener.cs
@@ -3,13 +3,41 @@ using System.Collections;
 
 public class DropBoxListener : SimpleDragListener
 {
+	public AudioClip onMouseOver;
+	public AudioClip onDrop;
+	private AudioSource source;
+	private bool hasPlayedEnter = false;
+
 	void Start()
 	{
+		source = gameObject.AddComponent<AudioSource>();
+
 		GameObject.FindObjectOfType<ListenerManager>().Register(this);
 	}
-	
+
+	void OnMouseEnter()
+	{
+		if (onMouseOver != null && !hasPlayedEnter)
+		{
+			hasPlayedEnter = true;
+			source.clip = onMouseOver;
+			source.Play();
+		}
+	}
+
+	void OnMouseExit()
+	{
+		hasPlayedEnter = false;
+	}
+
 	public override void OnDrop (GameObject drop)
 	{
+		if (onDrop != null)
+		{
+			source.clip = onDrop;
+			source.Play();
+		}
+
 		if (drop.GetComponent<ModifyStats>() != null)
 			drop.GetComponent<ModifyStats>().modify();
 

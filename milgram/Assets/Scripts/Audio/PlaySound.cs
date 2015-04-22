98bad6f5175a56ace9f2688ac33a56e6e3d97e85
smorris383 <smorris383@gmail.com>
2015-04-22 00:10:03 -0600

    Printing Animation Complete.

---
+++ milgram/Assets/Scripts/Audio/PlaySound.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlaySound : MonoBehaviour {
+	void Start() {
+		AudioSource audio = GetComponent<AudioSource>();
+		audio.Play();
+		audio.Play(44100);
+	}
+}

8d018c26729043e3cc513a874228ae745341104f
TrevorNewell <u0774750@utah.edu>
2015-05-02 16:05:18 -0600

    Time is now removed when a location is clicked.  Can specify individually.

---
+++ milgram/Assets/Scripts/Time/RemoveTimeOnClick.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoveTimeOnClick : MonoBehaviour 
+{
+	public int minutesToRemove;
+
+	void OnMouseUp()
+	{
+		Debug.Log("Subtracting " + minutesToRemove + " minutes.");
+		GameObject.FindObjectOfType<TimeManager>().SubtractTime(0, 0, minutesToRemove);
+	}
+}
